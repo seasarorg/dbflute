8b238e7ff1675d9c84b2146b2822be42efd1b923
jflute <dbflute@gmail.com>
2006-10-21 10:43:02 +0000

    Commit generate-tempalte.

---
+++ dbflute/etc/sample4use/ldb_fullProperties/generate.bat
@@ -0,0 +1,17 @@
+@echo off
+
+call _project.bat
+
+rem /nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
+rem Specify the file path to be used as build-properties.
+rem nnnnnnnnnn/
+set MY_PROPERTIES_PATH=build-%MY_PROJECT_NAME%.properties
+
+rem /nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
+rem Execute {Generate}.
+rem nnnnnnnnnn/
+call %DBFLUTE_HOME%\etc\cmd\_df-generate.cmd %MY_PROPERTIES_PATH%
+
+pause
+
+
