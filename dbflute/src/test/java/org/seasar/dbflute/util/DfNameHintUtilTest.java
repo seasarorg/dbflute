f9557794962ff7a4a7f3902e89efc9fdcea9ad0a
jflute <dbflute@gmail.com>
2006-11-19 03:28:31 +0000

    Refatoring

--- dbflute/src/test/java/org/seasar/dbflute/util/DfNameHintUtilTest.java
+++ dbflute/src/test/java/org/seasar/dbflute/util/DfNameHintUtilTest.java
@@ -1,35 +1,17 @@
-package org.apache.torque.helper.stateless;
+package org.seasar.dbflute.util;
 
-import java.io.BufferedWriter;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
-
-import org.apache.velocity.app.Velocity;
-import org.apache.velocity.texen.Generator;
-import org.apache.velocity.texen.util.FileUtil;
-import org.apache.velocity.util.StringUtils;
 import org.junit.Assert;
 import org.junit.Test;
 import org.seasar.dbflute.util.DfNameHintUtil;
 
 
-public class NameHintUtilTest {
+public class DfNameHintUtilTest {
 
-    public NameHintUtilTest() {
+    public DfNameHintUtilTest() {
     }
 
     @Test
     public void test_isHitByTheHint() {
-        try {
-            Velocity.init();
-        } catch (Exception e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        }
-        
         Assert.assertTrue(DfNameHintUtil.isHitByTheHint("XXX_YN", DfNameHintUtil.SUFFIX_MARK + "_YN"));
         Assert.assertTrue(DfNameHintUtil.isHitByTheHint("L_XXX", DfNameHintUtil.PREFIX_MARK + "L_"));
         Assert.assertTrue(DfNameHintUtil.isHitByTheHint("XXX", "XXX"));
