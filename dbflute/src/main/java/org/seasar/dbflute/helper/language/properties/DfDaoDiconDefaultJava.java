7afca26e4be0b0c55df12ba2e8fbf904665c67b9
jflute <dbflute@gmail.com>
2007-02-14 17:01:34 +0000

    j2eeResourceのDefault値をjdbc.diconからj2ee.diconに戻す。

--- dbflute/src/main/java/org/seasar/dbflute/helper/language/properties/DfDaoDiconDefaultJava.java
+++ dbflute/src/main/java/org/seasar/dbflute/helper/language/properties/DfDaoDiconDefaultJava.java
@@ -2,7 +2,7 @@ package org.seasar.dbflute.helper.language.properties;
 
 public class DfDaoDiconDefaultJava implements DfDaoDiconDefault {
     public String getJdbcDiconResourceName() {
-        return "jdbc.dicon";
+        return "j2ee.dicon";
     }
 
     public String getRequiredTxComponentName() {
