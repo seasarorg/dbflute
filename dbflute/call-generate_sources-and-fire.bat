6c7f00e4754ace13e456b75f89ba4de9d6e4224d
jflute <dbflute@gmail.com>
2007-05-31 10:51:17 +0000

    Implement TokenFile

---
+++ dbflute/call-generate_sources-and-fire.bat
@@ -0,0 +1,32 @@
+
+call mvn generate-sources
+
+
+cd ..\dbflute_ldb\dbflute
+
+@echo off
+
+call _project.bat
+
+rem /nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
+rem Specify the file path to be used as build-properties.
+rem nnnnnnnnnn/
+set MY_PROPERTIES_PATH=build-%MY_PROJECT_NAME%.properties
+
+rem /nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
+rem Execute {JDBC}.
+rem nnnnnnnnnn/
+call %DBFLUTE_HOME%\etc\cmd\_df-jdbc.cmd %MY_PROPERTIES_PATH%
+
+rem /nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
+rem Execute {Document}.
+rem nnnnnnnnnn/
+call %DBFLUTE_HOME%\etc\cmd\_df-doc.cmd %MY_PROPERTIES_PATH%
+
+rem /nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
+rem Execute {Generate}.
+rem nnnnnnnnnn/
+call %DBFLUTE_HOME%\etc\cmd\_df-generate.cmd %MY_PROPERTIES_PATH%
+
+
+pause
