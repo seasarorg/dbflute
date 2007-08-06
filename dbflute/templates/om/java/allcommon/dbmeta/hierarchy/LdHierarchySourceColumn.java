59035bda29fdfa35788e8b18e4ce80512f1bb5e6
jflute <dbflute@gmail.com>
2007-08-06 16:37:30 +0000

    Replace

--- dbflute/templates/om/java/allcommon/dbmeta/hierarchy/LdHierarchySourceColumn.java
+++
@@ -1,9 +0,0 @@
-package dbflute.ldb.allcommon.dbmeta.hierarchy;
-
-/**
- * @author jflute
- */
-public interface LdHierarchySourceColumn {
-
-    public String getColumnName();
-}
--- dbflute/templates/om/java/allcommon/dbmeta/hierarchy/basic/LdHierarchySourceRowSetupper.java
+++
@@ -1,8 +0,0 @@
-package dbflute.ldb.allcommon.dbmeta.hierarchy.basic;
-
-import dbflute.ldb.allcommon.dbmeta.hierarchy.LdHierarchySourceRow;
-
-public interface LdHierarchySourceRowSetupper<SOURCE_ROW> {
-
-    public LdHierarchySourceRow setup(SOURCE_ROW source);
-}
