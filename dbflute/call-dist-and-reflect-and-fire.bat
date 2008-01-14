c215f7a59c9ca4b8d15237c390d9e4a50296269b
jflute <dbflute@gmail.com>
2008-01-14 11:30:15 +0000

    [DBFLUTE-173]{Java}: SQL解析や検索結果マッピング時のパフォーマンス向上

--- dbflute/call-dist-and-reflect-and-fire.bat
+++ dbflute/call-dist-and-reflect-and-fire.bat
@@ -2,35 +2,7 @@ cd %~p0
 
 call ant -f build.xml dist
 
-call ant -f build.xml reflect
-
-
-
-cd ..\dfjava\dbflute_ldb
-
-@echo off
-
-call _project.bat
-
-rem /nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
-rem Specify the file path to be used as build-properties.
-rem nnnnnnnnnn/
-set MY_PROPERTIES_PATH=build-%MY_PROJECT_NAME%.properties
-
-rem /nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
-rem Execute {JDBC}.
-rem nnnnnnnnnn/
-call %DBFLUTE_HOME%\etc\cmd\_df-jdbc.cmd %MY_PROPERTIES_PATH%
-
-rem /nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
-rem Execute {Document}.
-rem nnnnnnnnnn/
-call %DBFLUTE_HOME%\etc\cmd\_df-doc.cmd %MY_PROPERTIES_PATH%
-
-rem /nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
-rem Execute {Generate}.
-rem nnnnnnnnnn/
-call %DBFLUTE_HOME%\etc\cmd\_df-generate.cmd %MY_PROPERTIES_PATH%
+call ant -f build.xml reflectvarious
 
 
 cd ..\..\dfncsharp\dbflute_nldb
@@ -60,6 +32,22 @@ rem nnnnnnnnnn/
 call %DBFLUTE_HOME%\etc\cmd\_df-generate.cmd %MY_PROPERTIES_PATH%
 
 
+cd ..\..\dbflute-oracle-example\dbflute_exampledb
+
+@echo off
+
+call jdbc.bat
+call doc.bat
+call generate.bat
+
+
+cd ..\..\dbflute-nbasic-example\dbflute_exampledb
+
+@echo off
+
+call jdbc.bat
+call doc.bat
+call generate.bat
 
 
 pause
