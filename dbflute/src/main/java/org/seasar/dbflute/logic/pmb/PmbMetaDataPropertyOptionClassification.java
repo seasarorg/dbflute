e3a7c9c2fa871094d3e41ae2a5e4763563cbf878
jflute <dbflute@gmail.com>
2008-02-05 15:47:38 +0000

    [DBFLUTE-195]{Java}: ParameterBeanでClassificationが利用できるようにする。

--- dbflute/src/main/java/org/seasar/dbflute/logic/pmb/PmbMetaDataPropertyOptionClassification.java
+++ dbflute/src/main/java/org/seasar/dbflute/logic/pmb/PmbMetaDataPropertyOptionClassification.java
@@ -10,6 +10,10 @@ import org.seasar.dbflute.properties.DfClassificationProperties;
  * @since 0.6.3 (2008/02/05 Tuesday)
  */
 public class PmbMetaDataPropertyOptionClassification {
+
+    protected static final String OPTION_PREFIX = "cls(";
+    protected static final String OPTION_SUFFIX = ")";
+
     protected String _className;
     protected String _propertyName;
     protected DfClassificationProperties _classificationProperties;
@@ -33,8 +37,9 @@ public class PmbMetaDataPropertyOptionClassification {
         final List<Map<String, String>> classificationMapList = _classificationProperties
                 .getClassificationMapList(classificationName);
         if (classificationMapList == null) {
-            String msg = "The classification was Not Found: " + _className + " " + _propertyName + ":cls("
-                    + classificationName + ")";
+            String msg = "The classification was Not Found:";
+            msg = msg + " " + _className + " " + _propertyName;
+            msg = msg + ":" + OPTION_PREFIX + classificationName + OPTION_SUFFIX;
             throw new IllegalStateException(msg);
         }
         return _classificationProperties.getClassificationMapList(classificationName);
@@ -43,9 +48,9 @@ public class PmbMetaDataPropertyOptionClassification {
     protected String extractClassificationNameFromOption(String className, String propertyName, boolean check) {
         final String pmbMetaDataPropertyOption = getPmbMetaDataPropertyOption();
         if (pmbMetaDataPropertyOption == null) {
-            System.out.println("1: " + className + "." + propertyName);
             if (check) {
-                String msg = "The property name don't have Option: " + className + "." + propertyName;
+                String msg = "The property name don't have Option:";
+                msg = msg + " " + className + "." + propertyName;
                 throw new IllegalStateException(msg);
             } else {
                 return null;
@@ -53,27 +58,26 @@ public class PmbMetaDataPropertyOptionClassification {
         }
         final String option = pmbMetaDataPropertyOption.trim();
         if (option.length() == 0) {
-            System.out.println("2: " + className + "." + propertyName);
             if (check) {
-                String msg = "The option of the property name should not be empty: " + className + "."
-                        + propertyName;
+                String msg = "The option of the property name should not be empty:";
+                msg = msg + " " + className + "." + propertyName;
                 throw new IllegalStateException(msg);
             } else {
                 return null;
             }
         }
-        if (option.startsWith("cls(") && option.endsWith(")")) {
-            System.out.println("3: " + className + "." + propertyName + ":{" + option + "}");
+        if (!option.startsWith(OPTION_PREFIX) || !option.endsWith(")")) {
             if (check) {
-                String msg = "The option of class name and the property name should be 'cls(xxx)': " + className
-                        + "." + propertyName + " - " + option;
+                String msg = "The option of class name and the property name should be 'cls(xxx)':";
+                msg = msg + " " + className + "." + propertyName + ":" + option;
                 throw new IllegalStateException(msg);
             } else {
                 return null;
             }
         }
-        return option.substring("cls(".length())
-                + option.substring("cls(".length(), option.length() - ")".length());
+        final int clsIdx = OPTION_PREFIX.length();
+        final int clsEndIdx = option.length() - OPTION_SUFFIX.length();
+        return option.substring(clsIdx) + option.substring(clsIdx, clsEndIdx);
     }
 
     protected String getPmbMetaDataPropertyOption() {
