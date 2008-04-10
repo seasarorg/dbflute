c127cbe8a85a0f087b8a7fbcdcf204b35061649e
jflute <dbflute@gmail.com>
2008-04-10 23:11:21 +0000

    Refactor.

--- dbflute/etc/client_directory-template/otherORMapper/_project.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-set MY_PROJECT_NAME=minimumProperties
-
-rem set DBFLUTE_HOME=..\mydbflute\dbflute-x.x.x
--- dbflute/etc/client_directory-template/otherORMapper/_project.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-export MY_PROJECT_NAME=ldb
-
-#export DBFLUTE_HOME=../mydbflute/dbflute-x.x.x
