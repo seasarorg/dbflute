8717e3a690fa9400aaaae5c63edb2180976689aa
jflute <dbflute@gmail.com>
2008-10-20 03:55:38 +0000

    (DBFLUTE-358){Java}: PostgreSQLのOID型に対応する

--- dbflute/util/deploy-as-limit-basic.bat
+++ dbflute/util/deploy-as-limit-basic.bat
@@ -1,4 +1,5 @@
 cd %~p0
+cd ..
 
 call .\ant\bin\ant -f build.xml dist
 call .\ant\bin\ant -f build.xml reflect-win-limit-basic
---
+++ dbflute/util/deploy-as-limit-postgresql.bat
@@ -0,0 +1,7 @@
+cd %~p0
+cd ..
+
+call .\ant\bin\ant -f build.xml dist
+call .\ant\bin\ant -f build.xml reflect-win-limit-postgresql
+
+pause
