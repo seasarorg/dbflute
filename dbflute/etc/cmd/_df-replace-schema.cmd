f11a5f7621d68fd0c22ec8625888ffa03a6208a3
jflute <dbflute@gmail.com>
2008-12-26 13:27:55 +0000

    Refactor replace-schema shell script.

--- dbflute/etc/cmd/_df-replace-schema.cmd
+++ dbflute/etc/cmd/_df-replace-schema.cmd
@@ -4,6 +4,6 @@ IF "%DBFLUTE_ENVIRONMENT_TYPE%"=="" set DBFLUTE_ENVIRONMENT_TYPE=""
 
 call %DBFLUTE_HOME%\etc\cmd\_df-copy-properties.cmd %NATIVE_PROPERTIES_PATH%
 
-IF "%answer%"=="" SET /P answer=Database will be initialized. Are you ready? (y or n)
+IF "%answer%"=="" SET /P answer=Database will be initialized. Are you ready? (y or n) 
 IF "%answer%"=="y" call %DBFLUTE_HOME%\ant\bin\ant -Ddfenv=%DBFLUTE_ENVIRONMENT_TYPE% -f %DBFLUTE_HOME%\build-torque.xml replace-schema
 
