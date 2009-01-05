05d7dcc6cfa0f4f2edebc21eeb5758d67d047d29
jflute <dbflute@gmail.com>
2009-01-05 08:16:03 +0000

    (DBFLUTE-409){C#}: ストアドプロシージャでNullable型の引数で例外発生

--- dbflute/util/deploy-as-limit-noracle.bat
+++ dbflute/util/deploy-as-limit-noracle.bat
@@ -1,7 +1,6 @@
 cd %~p0
 cd ..
 
-rem call .\ant\bin\ant -f build.xml dist
-call .\ant\bin\ant -f build.xml reflect-win-limit-noracle
+call .\ant\bin\ant -f buildnet.xml reflect-win-limit-noracle
 
 pause
