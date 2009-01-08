4ef6c05917352865edcb498350735556401838d6
jflute <dbflute@gmail.com>
2009-01-08 03:09:21 +0000

    (DBFLUTE-408){Java/C#}: DBFluteプロパティ(設定)をdfpropに寄せる

--- dbflute/etc/client-template/fullProperties/_project.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-set ANT_OPTS=-Xmx256M
-
-set MY_PROJECT_NAME=fullProperties
-
-rem set DBFLUTE_HOME=..\mydbflute\dbflute-x.x.x
--- dbflute/etc/client-template/fullProperties/_project.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-export ANT_OPTS=-Xmx256m
-
-export MY_PROJECT_NAME=fullProperties
-
-#export DBFLUTE_HOME=../mydbflute/dbflute-x.x.x
