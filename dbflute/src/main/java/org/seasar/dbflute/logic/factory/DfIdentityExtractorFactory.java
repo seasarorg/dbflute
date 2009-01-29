32cb2ff9541ff1f01dfc5a910bd1e7c6e398441c
jflute <dbflute@gmail.com>
2009-01-29 22:54:33 +0000

    (DBFLUTE-425){Java/C#}: DB2のIdentity情報をSYSCAT.COLUMNS表から

---
+++ dbflute/src/main/java/org/seasar/dbflute/logic/factory/DfIdentityExtractorFactory.java
@@ -0,0 +1,38 @@
+package org.seasar.dbflute.logic.factory;
+
+import javax.sql.DataSource;
+
+import org.seasar.dbflute.helper.jdbc.metadata.identity.DfIdentityExtractor;
+import org.seasar.dbflute.helper.jdbc.metadata.identity.DfIdentityExtractorDB2;
+import org.seasar.dbflute.properties.DfBasicProperties;
+
+/**
+ * @author jflute
+ * @since 0.8.1 (2008/10/10 Friday)
+ */
+public class DfIdentityExtractorFactory {
+
+    protected DfBasicProperties _basicProperties;
+    protected DataSource _dataSource;
+
+    /**
+     * @param basicProperties The basic properties. (NotNull)
+     * @param dataSource The data source. (NotNull)
+     */
+    public DfIdentityExtractorFactory(DfBasicProperties basicProperties, DataSource dataSource) {
+        _basicProperties = basicProperties;
+        _dataSource = dataSource;
+    }
+
+    /**
+     * @return The extractor of DB comments. (Nullable)
+     */
+    public DfIdentityExtractor createIdentityExtractor() {
+        if (_basicProperties.isDatabaseDB2()) {
+            final DfIdentityExtractorDB2 extractor = new DfIdentityExtractorDB2();
+            extractor.setDataSource(_dataSource);
+            return extractor;
+        }
+        return null;
+    }
+}
