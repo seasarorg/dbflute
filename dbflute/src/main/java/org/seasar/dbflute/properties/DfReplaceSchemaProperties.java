84e0180ba3febdbad951f2f0639f1bf001d01237
jflute <dbflute@gmail.com>
2009-03-03 09:05:25 +0000

    (DBFLUTE-446){Java/C#}: ReplaceSchemaでOracleで接続ユーザとスキーマが違う場合に例外

--- dbflute/src/main/java/org/seasar/dbflute/properties/DfReplaceSchemaProperties.java
+++ dbflute/src/main/java/org/seasar/dbflute/properties/DfReplaceSchemaProperties.java
@@ -140,90 +140,27 @@ public final class DfReplaceSchemaProperties extends DfAbstractHelperProperties 
     }
 
     // ===================================================================================
-    //                                                                      Once More Drop
-    //                                                                      ==============
-    @SuppressWarnings("unchecked")
-    protected Map<String, Object> getOnceMoreDropDefinitionMap() {
-        final Map<String, Object> map = (Map<String, Object>) getReplaceSchemaDefinitionMap().get(
-                "onceMoreDropDefinitionMap");
-        if (map != null) {
-            return map;
+    //                                                                   SQL File Encoding
+    //                                                                   =================
+    public String getSqlFileEncoding() {
+        final String sqlFileEncoding = (String) getReplaceSchemaDefinitionMap().get("sqlFileEncoding");
+        if (sqlFileEncoding != null && sqlFileEncoding.trim().length() != 0) {
+            return sqlFileEncoding;
         } else {
-            return new HashMap<String, Object>();
+            return "UTF-8";
         }
     }
 
-    public String getOnceMoreDropDefinitionSchema() {
-        final Map<String, Object> map = getOnceMoreDropDefinitionMap();
-        final Object obj = map.get("schema");
-        if (obj == null) {
+    // ===================================================================================
+    //                                                                          Skip Sheet
+    //                                                                          ==========
+    public String getSkipSheet() {
+        final String skipSheet = (String) getReplaceSchemaDefinitionMap().get("skipSheet");
+        if (skipSheet != null && skipSheet.trim().length() != 0) {
+            return skipSheet;
+        } else {
             return null;
         }
-        if (!(obj instanceof String)) {
-            String msg = "The schema should be String: schema=" + obj + " type=" + obj.getClass();
-            throw new IllegalStateException(msg);
-        }
-        return (String) obj;
-    }
-
-    @SuppressWarnings("unchecked")
-    public List<String> getOnceMoreDropObjectTypeList() {
-        final Map<String, Object> map = getOnceMoreDropDefinitionMap();
-        Object obj = map.get("objectTypeList");
-        if (obj == null) {
-            obj = map.get("targetDatabaseTypeList");
-            if (obj == null) {
-                ArrayList<String> defaultList = new ArrayList<String>();
-                defaultList.add("TABLE");
-                defaultList.add("VIEW");
-                return defaultList;
-            }
-        }
-        if (!(obj instanceof List)) {
-            String msg = "The schema should be List<String>: objectTypeList=" + obj + " type=" + obj.getClass();
-            throw new IllegalStateException(msg);
-        }
-        return (List<String>) obj;
-    }
-
-    @SuppressWarnings("unchecked")
-    public List<String> getOnceMoreDropTableTargetList() {
-        final Map<String, Object> map = getOnceMoreDropDefinitionMap();
-        Object obj = map.get("tableTargetList");
-        if (obj == null) {
-            return new ArrayList<String>();
-        }
-        if (!(obj instanceof List)) {
-            String msg = "The schema should be List<String>: tableTargetList=" + obj + " type=" + obj.getClass();
-            throw new IllegalStateException(msg);
-        }
-        return (List<String>) obj;
-    }
-
-    @SuppressWarnings("unchecked")
-    public List<String> getOnceMoreDropTableExceptList() {
-        final Map<String, Object> map = getOnceMoreDropDefinitionMap();
-        Object obj = map.get("tableExceptList");
-        if (obj == null) {
-            return new ArrayList<String>();
-        }
-        if (!(obj instanceof List)) {
-            String msg = "The schema should be List<String>: tableExceptList=" + obj + " type=" + obj.getClass();
-            throw new IllegalStateException(msg);
-        }
-        return (List<String>) obj;
-    }
-
-    @SuppressWarnings("unchecked")
-    public boolean isOnceMoreDropAllTable() {
-        String value = (String) getOnceMoreDropDefinitionMap().get("dropAllTable");
-        if (value == null) {
-            value = (String) getOnceMoreDropDefinitionMap().get("isDropAllTable");
-            if (value == null) {
-                return false;
-            }
-        }
-        return value.equalsIgnoreCase("true");
     }
 
     // ===================================================================================
@@ -312,8 +249,95 @@ public final class DfReplaceSchemaProperties extends DfAbstractHelperProperties 
     }
 
     // ===================================================================================
-    //                                                                               Other
-    //                                                                               =====
+    //                                                                      Once More Drop
+    //                                                                      ==============
+    @SuppressWarnings("unchecked")
+    protected Map<String, Object> getOnceMoreDropDefinitionMap() {
+        final Map<String, Object> map = (Map<String, Object>) getReplaceSchemaDefinitionMap().get(
+                "onceMoreDropDefinitionMap");
+        if (map != null) {
+            return map;
+        } else {
+            return new HashMap<String, Object>();
+        }
+    }
+
+    public String getOnceMoreDropDefinitionSchema() {
+        final Map<String, Object> map = getOnceMoreDropDefinitionMap();
+        final Object obj = map.get("schema");
+        if (obj == null) {
+            return null;
+        }
+        if (!(obj instanceof String)) {
+            String msg = "The schema should be String: schema=" + obj + " type=" + obj.getClass();
+            throw new IllegalStateException(msg);
+        }
+        return (String) obj;
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<String> getOnceMoreDropObjectTypeList() {
+        final Map<String, Object> map = getOnceMoreDropDefinitionMap();
+        Object obj = map.get("objectTypeList");
+        if (obj == null) {
+            obj = map.get("targetDatabaseTypeList");
+            if (obj == null) {
+                ArrayList<String> defaultList = new ArrayList<String>();
+                defaultList.add("TABLE");
+                defaultList.add("VIEW");
+                return defaultList;
+            }
+        }
+        if (!(obj instanceof List)) {
+            String msg = "The schema should be List<String>: objectTypeList=" + obj + " type=" + obj.getClass();
+            throw new IllegalStateException(msg);
+        }
+        return (List<String>) obj;
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<String> getOnceMoreDropTableTargetList() {
+        final Map<String, Object> map = getOnceMoreDropDefinitionMap();
+        Object obj = map.get("tableTargetList");
+        if (obj == null) {
+            return new ArrayList<String>();
+        }
+        if (!(obj instanceof List)) {
+            String msg = "The schema should be List<String>: tableTargetList=" + obj + " type=" + obj.getClass();
+            throw new IllegalStateException(msg);
+        }
+        return (List<String>) obj;
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<String> getOnceMoreDropTableExceptList() {
+        final Map<String, Object> map = getOnceMoreDropDefinitionMap();
+        Object obj = map.get("tableExceptList");
+        if (obj == null) {
+            return new ArrayList<String>();
+        }
+        if (!(obj instanceof List)) {
+            String msg = "The schema should be List<String>: tableExceptList=" + obj + " type=" + obj.getClass();
+            throw new IllegalStateException(msg);
+        }
+        return (List<String>) obj;
+    }
+
+    @SuppressWarnings("unchecked")
+    public boolean isOnceMoreDropAllTable() {
+        String value = (String) getOnceMoreDropDefinitionMap().get("dropAllTable");
+        if (value == null) {
+            value = (String) getOnceMoreDropDefinitionMap().get("isDropAllTable");
+            if (value == null) {
+                return false;
+            }
+        }
+        return value.equalsIgnoreCase("true");
+    }
+    
+    // ===================================================================================
+    //                                                                        Other Closet
+    //                                                                        ============
     public boolean isAutoCommit() { // It's closet!
         return analyzeBooleanProperty("isAutoCommit", true);
     }
@@ -336,24 +360,6 @@ public final class DfReplaceSchemaProperties extends DfAbstractHelperProperties 
     
     public boolean isSuppressDropTable() { // It's closet!
         return analyzeBooleanProperty("isSuppressDropTable", false);
-    }
-
-    public String getSqlFileEncoding() {
-        final String sqlFileEncoding = (String) getReplaceSchemaDefinitionMap().get("sqlFileEncoding");
-        if (sqlFileEncoding != null && sqlFileEncoding.trim().length() != 0) {
-            return sqlFileEncoding;
-        } else {
-            return "UTF-8";
-        }
-    }
-
-    public String getSkipSheet() {
-        final String skipSheet = (String) getReplaceSchemaDefinitionMap().get("skipSheet");
-        if (skipSheet != null && skipSheet.trim().length() != 0) {
-            return skipSheet;
-        } else {
-            return null;
-        }
     }
 
     // ===================================================================================
