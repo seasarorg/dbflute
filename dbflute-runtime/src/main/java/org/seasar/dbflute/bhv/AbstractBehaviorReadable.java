2feab00c4212b42b856c03d3b6e97e32305ecc8b
jflute <dbflute@gmail.com>
2009-06-20 04:02:38 +0000

    Use Find Bugs

--- dbflute-runtime/src/main/java/org/seasar/dbflute/bhv/AbstractBehaviorReadable.java
+++ dbflute-runtime/src/main/java/org/seasar/dbflute/bhv/AbstractBehaviorReadable.java
@@ -977,7 +977,7 @@ public abstract class AbstractBehaviorReadable implements BehaviorReadable {
      */
     protected void assertObjectNotNull(String variableName, Object value) {
         if (variableName == null) {
-            String msg = "The value should not be null: variableName=" + variableName + " value=" + value;
+            String msg = "The value should not be null: variableName=null value=" + value;
             throw new IllegalArgumentException(msg);
         }
         if (value == null) {
