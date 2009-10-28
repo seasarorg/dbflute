60fda5450070133196fb2c312ed43e05ffe4624a
jflute <dbflute@gmail.com>
2009-10-28 03:18:15 +0000

    Refactor map string libraries.

--- dbflute-runtime/src/main/java/org/seasar/dbflute/infra/dfprop/MapStringFileReader.java
+++ dbflute-runtime/src/main/java/org/seasar/dbflute/infra/dfprop/MapStringFileReader.java
@@ -27,25 +27,39 @@ import java.util.Map.Entry;
 import org.seasar.dbflute.helper.mapstring.impl.MapListStringImpl;
 
 /**
+ * The reader for map string file.
  * @author jflute
  * @since 0.9.6 (2009/10/28 Wednesday)
  */
-public class MapStringFileReaderImpl {
+public class MapStringFileReader {
 
     // ===================================================================================
-    //                                                                           Attribute
-    //                                                                           =========
-    protected final String _lineCommentMark = "#";
+    //                                                                          Definition
+    //                                                                          ==========
+    protected static final String LINE_COMMENT_MARK = "#";
 
     // ===================================================================================
     //                                                                                Read
     //                                                                                ====
     /**
+     * Read the map string file. <br />
+     * If the type of values is various type, this method is available. <br />
+     * A trimmed line that starts with '#' is treated as line comment. <br />
+     * This is the most basic method here.
+     * <pre>
+     * map:{
+     *     ; key1 = string-value1
+     *     ; key2 = list:{element1 ; element2 }
+     *     ; key3 = map:{key1 = value1 ; key2 = value2 }
+     *     ; ... = ...
+     * }
+     * </pre>
      * @param path The file path. (NotNull)
      * @param encoding The file encoding. (NotNull)
      * @return The read map. (NotNull)
      */
     public Map<String, Object> readMap(String path, String encoding) {
+        final String lineCommentMark = getLineCommentMark();
         final File file = new File(path);
         final StringBuilder sb = new StringBuilder();
         if (file.exists()) {
@@ -72,7 +86,7 @@ public class MapStringFileReaderImpl {
                         continue;
                     }
                     // If the line is comment...
-                    if (_lineCommentMark != null && lineString.trim().startsWith(_lineCommentMark)) {
+                    if (lineCommentMark != null && lineString.trim().startsWith(lineCommentMark)) {
                         continue;
                     }
                     sb.append(lineString);
@@ -104,6 +118,22 @@ public class MapStringFileReaderImpl {
         return value;
     }
 
+    /**
+     * Read the map string file as string value. <br />
+     * If the type of all values is string type, this method is available. <br />
+     * A trimmed line that starts with '#' is treated as line comment.
+     * <pre>
+     * ex)
+     * map:{
+     *     ; key1 = string-value1
+     *     ; key2 = string-value2
+     *     ; ... = ...
+     * }
+     * </pre>
+     * @param path The file path. (NotNull)
+     * @param encoding The file encoding. (NotNull)
+     * @return The read map. (NotNull)
+     */
     public Map<String, String> readMapAsStringValue(String path, String encoding) {
         final Map<String, String> resultMap = new LinkedHashMap<String, String>();
         final Map<String, Object> map = readMap(path, encoding);
@@ -115,6 +145,22 @@ public class MapStringFileReaderImpl {
         return resultMap;
     }
 
+    /**
+     * Read the map string file as list string value. <br />
+     * If the type of all values is list string type, this method is available. <br />
+     * A trimmed line that starts with '#' is treated as line comment.
+     * <pre>
+     * ex)
+     * map:{
+     *     ; key1 = list:{string-element1 ; string-element2 ; ...}
+     *     ; key2 = list:{string-element1 ; string-element2 ; ...}
+     *     ; ... = list:{...}
+     * }
+     * </pre>
+     * @param path The file path. (NotNull)
+     * @param encoding The file encoding. (NotNull)
+     * @return The read map. (NotNull)
+     */
     @SuppressWarnings("unchecked")
     public Map<String, List<String>> readMapAsListStringValue(String path, String encoding) {
         final Map<String, List<String>> resultMap = newLinkedHashMap();
@@ -126,6 +172,22 @@ public class MapStringFileReaderImpl {
         return resultMap;
     }
 
+    /**
+     * Read the map string file as map string value. <br />
+     * If the type of all values is map string type, this method is available. <br />
+     * A trimmed line that starts with '#' is treated as line comment.
+     * <pre>
+     * ex)
+     * map:{
+     *     ; key1 = map:{string-key1 = string-value1 ; string-key2 = string-value2 }
+     *     ; key2 = map:{string-key1 = string-value1 ; string-key2 = string-value2 }
+     *     ; ... = map:{...}
+     * }
+     * </pre>
+     * @param path The file path. (NotNull)
+     * @param encoding The file encoding. (NotNull)
+     * @return The read map. (NotNull)
+     */
     @SuppressWarnings("unchecked")
     public Map<String, Map<String, String>> readMapAsMapStringValue(String path, String encoding) {
         final Map<String, Map<String, String>> resultMap = newLinkedHashMap();
@@ -136,6 +198,13 @@ public class MapStringFileReaderImpl {
         }
 
         return resultMap;
+    }
+
+    // ===================================================================================
+    //                                                                     Extension Point
+    //                                                                     ===============
+    protected String getLineCommentMark() {
+        return LINE_COMMENT_MARK;
     }
 
     // ===================================================================================
