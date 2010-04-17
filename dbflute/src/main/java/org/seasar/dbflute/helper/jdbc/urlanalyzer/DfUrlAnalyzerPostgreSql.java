5738bb9b2d0f3c8661ecd0ca7439e83ebf02d6df
jflute <dbflute@gmail.com>
2010-04-17 06:37:39 +0000

    (DBFLUTE-682){Java/C#}: additionalSchemaで別データベースのスキーマを指定

---
+++ dbflute/src/main/java/org/seasar/dbflute/helper/jdbc/urlanalyzer/DfUrlAnalyzerPostgreSql.java
@@ -0,0 +1,25 @@
+package org.seasar.dbflute.helper.jdbc.urlanalyzer;
+
+import org.seasar.dbflute.util.Srl;
+
+/**
+ * 
+ * @author jflute
+ * @since 0.9.6 (2009/10/31 Saturday)
+ */
+public class DfUrlAnalyzerPostgreSql implements DfUrlAnalyzer {
+
+    protected String _url;
+
+    public DfUrlAnalyzerPostgreSql(String url) {
+        this._url = url;
+    }
+
+    public String extractCatalog() {
+        if (_url == null) {
+            return null;
+        }
+        final String pureUrl = Srl.substringFirstFront(_url, "?");
+        return Srl.substringFirstRear(pureUrl, "/");
+    }
+}
