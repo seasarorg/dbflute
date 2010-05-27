9e5e591a34b73ba282ac7ee5c163119135d7ab0a
jflute <dbflute@gmail.com>
2010-05-27 06:44:08 +0000

    (DBFLUTE-708){Java/C#}: SQLiteでCBでNullsFirst/Lastを利用できるように

--- dbflute/deploy-script/ossdb-deploy.sh
+++ dbflute/deploy-script/ossdb-deploy.sh
@@ -1,6 +1,7 @@
 cd ..
 ant -f build.xml reflect-to-mysql
 ant -f build.xml reflect-to-postgresql
+ant -f build.xml reflect-to-tricky
 
 cd ../dbflute-mysql-example/dbflute_exampledb
 rm ./log/*.log
@@ -11,6 +12,14 @@ rm ./log/*.log
 . outside-sql-test.sh
 
 cd ../../dbflute-postgresql-example/dbflute_exampledb
+rm ./log/*.log
+. jdbc.sh
+. doc.sh
+. generate.sh
+. sql2entity.sh
+. outside-sql-test.sh
+
+cd ../../dbflute-tricky-example/dbflute_exampledb
 rm ./log/*.log
 . jdbc.sh
 . doc.sh
