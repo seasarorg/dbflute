366af7c122aaeff4869eeddaf1f76d510b79bec0
jflute <dbflute@gmail.com>
2010-06-05 06:20:36 +0000

    (DBFlute-715){Java/C#}: ParameterBeanのプロパティ型のパッケージ解決でListやMapの要素も

--- dbflute/src/main/java/org/seasar/dbflute/logic/sql2entity/pmbean/DfStandardApiPackageResolver.java
+++ dbflute/src/main/java/org/seasar/dbflute/logic/sql2entity/pmbean/DfStandardApiPackageResolver.java
@@ -2,6 +2,8 @@ package org.seasar.dbflute.logic.sql2entity.pmbean;
 
 import org.seasar.dbflute.properties.DfBasicProperties;
 import org.seasar.dbflute.util.DfStringUtil;
+import org.seasar.dbflute.util.Srl;
+import org.seasar.dbflute.util.Srl.ScopeInfo;
 
 /**
  * @author jflute
@@ -36,41 +38,100 @@ public class DfStandardApiPackageResolver {
         if (typeName == null) {
             return typeName;
         }
-        final DfBasicProperties prop = _basicProperties;
-        if (prop.isTargetLanguageJava()) {
-            if (!exceptUtil) {
-                if (typeName.startsWith("List<") && typeName.endsWith(">")) {
-                    return "java.util." + typeName;
-                }
-                if (typeName.startsWith("Map<") && typeName.endsWith(">")) {
-                    return "java.util." + typeName;
-                }
+        if (isTargetLanguageJava()) {
+            final String processed = processJavaType(typeName, exceptUtil);
+            if (processed != null) {
+                return processed;
             }
-            if (typeName.equals("BigDecimal")) {
-                return "java.math." + typeName;
-            }
-            if (typeName.equals("Time")) {
-                return "java.sql." + typeName;
-            }
-            if (typeName.equals("Timestamp")) {
-                return "java.sql." + typeName;
-            }
-            if (!exceptUtil) {
-                if (typeName.equals("Date")) {
-                    return "java.util." + typeName;
-                }
-            }
-        } else if (prop.isTargetLanguageCSharp()) {
-            if (typeName.startsWith("IList<") && typeName.endsWith(">")) {
-                return "System.Collections.Generic." + typeName;
+        } else if (isTargetLanguageCSharp()) {
+            final String processed = processCSharpType(typeName, exceptUtil);
+            if (processed != null) {
+                return processed;
             }
         }
         if (typeName.startsWith("$$CDef$$")) {
-            String pkg = prop.getBaseCommonPackage();
-            String prefix = prop.getProjectPrefix();
+            final DfBasicProperties prop = _basicProperties;
+            final String pkg = prop.getBaseCommonPackage();
+            final String prefix = prop.getProjectPrefix();
             typeName = DfStringUtil.replace(typeName, "$$CDef$$", pkg + "." + prefix + "CDef");
             return typeName;
         }
         return typeName;
+    }
+
+    protected String processJavaType(String typeName, boolean exceptUtil) {
+        if (!exceptUtil) {
+            final String listType = processListType(typeName, exceptUtil, "java.util", "List");
+            if (listType != null) {
+                return listType;
+            }
+            final String mapType = processMapType(typeName, exceptUtil, "java.util", "Map");
+            if (mapType != null) {
+                return mapType;
+            }
+        }
+        if (typeName.equals("BigDecimal")) {
+            return "java.math." + typeName;
+        }
+        if (typeName.equals("Time")) {
+            return "java.sql." + typeName;
+        }
+        if (typeName.equals("Timestamp")) {
+            return "java.sql." + typeName;
+        }
+        if (!exceptUtil) {
+            if (typeName.equals("Date")) {
+                return "java.util." + typeName;
+            }
+        }
+        return null;
+    }
+
+    protected String processCSharpType(String typeName, boolean exceptUtil) {
+        final String listType = processListType(typeName, exceptUtil, "System.Collections.Generic", "IList");
+        if (listType != null) {
+            return listType;
+        }
+        final String mapType = processMapType(typeName, exceptUtil, "System.Collections.Generic", "IDictionary");
+        if (mapType != null) {
+            return mapType;
+        }
+        return null;
+    }
+
+    protected String processListType(String typeName, boolean exceptUtil, String listPkg, String listName) {
+        final String listBegin = listName + "<";
+        final String listEnd = ">";
+        if (typeName.startsWith(listBegin) && typeName.endsWith(listEnd)) {
+            final ScopeInfo scope = Srl.extractScopeWide(typeName, listBegin, listEnd);
+            final String content = scope.getContent();
+            final String resolvedContent = doResolvePackageName(content, exceptUtil);
+            return listPkg + "." + listBegin + resolvedContent + listEnd;
+        } else {
+            return null;
+        }
+    }
+
+    protected String processMapType(String typeName, boolean exceptUtil, String mapPkg, String mapName) {
+        final String mapBegin = mapName + "<";
+        final String mapEnd = ">";
+        if (typeName.startsWith(mapBegin) && typeName.endsWith(mapEnd)) {
+            final ScopeInfo scope = Srl.extractScopeWide(typeName, mapBegin, mapEnd);
+            final String content = scope.getContent();
+            final String keyType = Srl.substringFirstFront(content, ",").trim();
+            final String valueType = Srl.substringFirstRear(content, ",").trim();
+            final String resolvedValueType = doResolvePackageName(valueType, exceptUtil);
+            return mapPkg + "." + mapBegin + keyType + ", " + resolvedValueType + mapEnd;
+        } else {
+            return null;
+        }
+    }
+
+    protected boolean isTargetLanguageJava() {
+        return _basicProperties.isTargetLanguageJava();
+    }
+
+    protected boolean isTargetLanguageCSharp() {
+        return _basicProperties.isTargetLanguageCSharp();
     }
 }
---
+++ dbflute/src/test/java/org/seasar/dbflute/logic/sql2entity/pmbean/DfStandardApiPackageResolverTest.java
@@ -0,0 +1,123 @@
+package org.seasar.dbflute.logic.sql2entity.pmbean;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.seasar.dbflute.unit.PlainTestCase;
+
+/**
+ * @author jflute
+ */
+public class DfStandardApiPackageResolverTest extends PlainTestCase {
+
+    @Test
+    public void test_doResolvePackageName_Java_Date_basic() throws Exception {
+        // ## Arrange ##
+        DfStandardApiPackageResolver resolver = createJavaTarget();
+
+        // ## Act & Assert ##
+        assertEquals("java.util.Date", resolver.doResolvePackageName("Date", false));
+        assertEquals("Date", resolver.doResolvePackageName("Date", true));
+    }
+
+    @Test
+    public void test_doResolvePackageName_Java_List_basic() throws Exception {
+        // ## Arrange ##
+        DfStandardApiPackageResolver resolver = createJavaTarget();
+
+        // ## Act & Assert ##
+        assertEquals("java.util.List<String>", resolver.doResolvePackageName("List<String>", false));
+        assertEquals("List<String>", resolver.doResolvePackageName("List<String>", true));
+    }
+
+    @Test
+    public void test_doResolvePackageName_Java_List_nest() throws Exception {
+        // ## Arrange ##
+        DfStandardApiPackageResolver resolver = createJavaTarget();
+
+        // ## Act ##
+        String actual = resolver.doResolvePackageName("List<List<Date>>", false);
+
+        // ## Assert ##
+        assertEquals("java.util.List<java.util.List<java.util.Date>>", actual);
+    }
+
+    @Test
+    public void test_doResolvePackageName_Java_List_nest_Map() throws Exception {
+        // ## Arrange ##
+        DfStandardApiPackageResolver resolver = createJavaTarget();
+
+        // ## Act ##
+        String actual = resolver.doResolvePackageName("List<List<Map<Date, Date>>>", false);
+
+        // ## Assert ##
+        log(actual);
+        assertEquals("java.util.List<java.util.List<java.util.Map<Date, java.util.Date>>>", actual);
+    }
+
+    @Test
+    public void test_doResolvePackageName_Java_Map_basic() throws Exception {
+        // ## Arrange ##
+        DfStandardApiPackageResolver resolver = createJavaTarget();
+
+        // ## Act & Assert ##
+        assertEquals("java.util.Map<String, String>", resolver.doResolvePackageName("Map<String, String>", false));
+        assertEquals("java.util.Map<String, java.util.Date>", resolver.doResolvePackageName("Map<String, Date>", false));
+    }
+
+    @Test
+    public void test_doResolvePackageName_Java_Map_nest() throws Exception {
+        // ## Arrange ##
+        DfStandardApiPackageResolver resolver = createJavaTarget();
+
+        // ## Act ##
+        String actual = resolver.doResolvePackageName("Map<Date, List<Map<Date, Date>>>", false);
+
+        // ## Assert ##
+        log(actual);
+        assertEquals("java.util.Map<Date, java.util.List<java.util.Map<Date, java.util.Date>>>", actual);
+    }
+
+    @Test
+    public void test_doResolvePackageName_CSharp_List_nest_Map() throws Exception {
+        // ## Arrange ##
+        DfStandardApiPackageResolver resolver = createCSharpTarget();
+
+        // ## Act ##
+        String actual = resolver.doResolvePackageName("IList<IList<IDictionary<Date, Date>>>", false);
+
+        // ## Assert ##
+        log(actual);
+        assertEquals(
+                "System.Collections.Generic.IList<System.Collections.Generic.IList<System.Collections.Generic.IDictionary<Date, Date>>>",
+                actual);
+    }
+
+    protected DfStandardApiPackageResolver createJavaTarget() {
+        return new DfStandardApiPackageResolver(null) {
+            @Override
+            protected boolean isTargetLanguageJava() {
+                return true;
+            }
+
+            @Override
+            protected boolean isTargetLanguageCSharp() {
+                return false;
+            }
+        };
+    }
+
+    protected DfStandardApiPackageResolver createCSharpTarget() {
+        return new DfStandardApiPackageResolver(null) {
+            @Override
+            protected boolean isTargetLanguageJava() {
+                return false;
+            }
+
+            @Override
+            protected boolean isTargetLanguageCSharp() {
+                return true;
+            }
+        };
+    }
+}
