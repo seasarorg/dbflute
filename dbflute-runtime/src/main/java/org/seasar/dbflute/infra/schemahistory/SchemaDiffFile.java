0ec2760ca3ebc0317bbb54c6dd31a661db2585ed
jflute <dbflute@gmail.com>
2010-06-07 08:19:10 +0000

    (DBFLUTE-588){Java/C#}: DB変更の内容を表示する差分ドキュメント

--- dbflute-runtime/src/main/java/org/seasar/dbflute/infra/schemahistory/SchemaDiffFile.java
+++ dbflute-runtime/src/main/java/org/seasar/dbflute/infra/schemahistory/SchemaDiffFile.java
@@ -13,7 +13,7 @@
  * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-package org.seasar.dbflute.infra.schemadiff;
+package org.seasar.dbflute.infra.schemahistory;
 
 import java.io.InputStream;
 import java.io.OutputStream;
