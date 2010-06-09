4290371e03175af2ffbce269c79fe95d6f433095
jflute <dbflute@gmail.com>
2010-06-09 13:05:25 +0000

    Add exception messages to procedure's process

--- dbflute/src/main/java/org/seasar/dbflute/logic/sql2entity/pmbean/DfParameterBeanBasicHandler.java
+++ dbflute/src/main/java/org/seasar/dbflute/logic/sql2entity/pmbean/DfParameterBeanBasicHandler.java
@@ -304,7 +304,7 @@ public class DfParameterBeanBasicHandler {
             if (metaInfo == null) {
                 return "";
             }
-            return " - " + metaInfo.getColumnDefinitionLineDisp();
+            return ": {" + metaInfo.getColumnDefinitionLineDisp() + "}";
         }
     }
 
