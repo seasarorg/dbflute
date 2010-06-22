8996d2d09e34378f7d29451e78e4d67eaa417204
jflute <dbflute@gmail.com>
2010-06-22 14:58:21 +0000

    (DBFLUTE-730){Java}: 相対値による更新(varyingUpdate)

--- dbflute-runtime/src/main/java/org/seasar/dbflute/cbean/sqlclause/AbstractSqlClause.java
+++ dbflute-runtime/src/main/java/org/seasar/dbflute/cbean/sqlclause/AbstractSqlClause.java
@@ -42,6 +42,7 @@ import org.seasar.dbflute.cbean.sqlclause.query.OrScopeQuerySetupper;
 import org.seasar.dbflute.cbean.sqlclause.query.QueryClause;
 import org.seasar.dbflute.cbean.sqlclause.query.QueryClauseFilter;
 import org.seasar.dbflute.cbean.sqlclause.query.StringQueryClause;
+import org.seasar.dbflute.cbean.sqlclause.select.SelectedSelectColumnInfo;
 import org.seasar.dbflute.cbean.sqlclause.subquery.SubQueryIndentProcessor;
 import org.seasar.dbflute.dbmeta.DBMeta;
 import org.seasar.dbflute.dbmeta.DBMetaProvider;
@@ -876,53 +877,6 @@ public abstract class AbstractSqlClause implements SqlClause, Serializable {
         return resultMap;
     }
 
-    public static class SelectedSelectColumnInfo {
-        protected String _tableAliasName;
-        protected String _columnDbName;
-        protected ColumnSqlName _columnSqlName;
-        protected String _columnAliasName;
-
-        public String buildRealColumnSqlName() {
-            if (_tableAliasName != null) {
-                return _tableAliasName + "." + _columnSqlName.toString();
-            } else {
-                return _columnSqlName.toString();
-            }
-        }
-
-        public String getTableAliasName() {
-            return _tableAliasName;
-        }
-
-        public void setTableAliasName(String tableAliasName) {
-            this._tableAliasName = tableAliasName;
-        }
-
-        public String getColumnDbName() {
-            return _columnDbName;
-        }
-
-        public void setColumnDbName(String columnName) {
-            this._columnDbName = columnName;
-        }
-
-        public ColumnSqlName getColumnSqlName() {
-            return _columnSqlName;
-        }
-
-        public void setColumnSqlName(ColumnSqlName columnSqlName) {
-            this._columnSqlName = columnSqlName;
-        }
-
-        public String getColumnAliasName() {
-            return _columnAliasName;
-        }
-
-        public void setColumnAliasName(String columnAliasName) {
-            this._columnAliasName = columnAliasName;
-        }
-    }
-
     // ===================================================================================
     //                                                                           OuterJoin
     //                                                                           =========
@@ -1293,10 +1247,10 @@ public abstract class AbstractSqlClause implements SqlClause, Serializable {
                 count++;
             }
         } catch (RuntimeException e) {
-            String msg = "registerOrderBy() threw the exception: orderByProperty=" + orderByProperty;
-            msg = msg + " ascOrDesc=" + ascOrDesc;
-            msg = msg + " sqlClause=" + this.toString();
-            throw new RuntimeException(msg, e);
+            String msg = "Failed to register order-by:";
+            msg = msg + " orderByProperty=" + orderByProperty + " ascOrDesc=" + ascOrDesc;
+            msg = msg + " table=" + _tableDbName;
+            throw new IllegalStateException(msg, e);
         }
     }
 
