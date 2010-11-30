e01f9ab5841338fc4d73cfed24bd7e735b03358b
jflute <dbflute@gmail.com>
2010-11-30 09:00:52 +0000

    (DBFLUTE-769){Java}: Sybaseでもある程度自動生成ができるようにする(若干サポート)

--- dbflute/src/test/java/org/seasar/dbflute/logic/jdbc/urlanalyzer/DfUrlAnalyzerSybaseTest.java
+++ dbflute/src/test/java/org/seasar/dbflute/logic/jdbc/urlanalyzer/DfUrlAnalyzerSybaseTest.java
@@ -23,6 +23,30 @@ public class DfUrlAnalyzerSybaseTest extends PlainTestCase {
         assertEquals("exampledb", catalog);
     }
 
+    @Test
+    public void test_extractCatalog_option_ampersand() throws Exception {
+        // ## Arrange ##
+        DfUrlAnalyzer analyzer = createTarget("jdbc:sybase:Tds:localhost:2638/exampledb&charSet=UTF-8");
+
+        // ## Act ##
+        String catalog = analyzer.extractCatalog();
+
+        // ## Assert ##
+        assertEquals("exampledb", catalog);
+    }
+
+    @Test
+    public void test_extractCatalog_option_question() throws Exception {
+        // ## Arrange ##
+        DfUrlAnalyzer analyzer = createTarget("jdbc:sybase:Tds:localhost:5432/exampledb?charSet=UTF-8");
+
+        // ## Act ##
+        String catalog = analyzer.extractCatalog();
+
+        // ## Assert ##
+        assertEquals("exampledb", catalog);
+    }
+
     protected DfUrlAnalyzer createTarget(String url) {
         return new DfUrlAnalyzerSybase(url);
     }
