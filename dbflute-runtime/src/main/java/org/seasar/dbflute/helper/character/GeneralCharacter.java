3acbbc921ab4f45f8b6889963f1efd22aa10c370
jflute <dbflute@gmail.com>
2011-05-03 00:12:40 +0000

    Refactor

--- dbflute-runtime/src/main/java/org/seasar/dbflute/helper/character/GeneralCharacter.java
+++ dbflute-runtime/src/main/java/org/seasar/dbflute/helper/character/GeneralCharacter.java
@@ -19,13 +19,93 @@ package org.seasar.dbflute.helper.character;
  * The interface of general character.
  * @author jflute
  */
-public interface GeneralCharacter {
+public class GeneralCharacter {
 
-    public String toSingleByteAlphabet(String s);
+    public String toSingleByteAlphabet(String target) {
+        if (target == null) {
+            return target;
+        }
+        final StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < target.length(); i++) {
+            final char currentChar = target.charAt(i);
 
-    public String toSingleByteNumber(String s);
+            if (currentChar >= 0xff21 && currentChar <= 0xff3a) {
+                sb.append(toSingleByteCharacter(currentChar));
+            } else if (currentChar >= 0xff41 && currentChar <= 0xff5a) {
+                sb.append(toSingleByteCharacter(currentChar));
+            } else {
+                sb.append(currentChar);
+            }
+        }
+        return sb.toString();
+    }
 
-    public String toSingleByteAlphabetNumber(String target);
+    public String toSingleByteNumber(String target) {
+        if (target == null) {
+            return target;
+        }
+        final StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < target.length(); i++) {
+            final char currentChar = target.charAt(i);
+            if (currentChar >= 0xff10 && currentChar <= 0xff19) {
+                sb.append(toSingleByteCharacter(currentChar));
+            } else {
+                sb.append(currentChar);
+            }
+        }
+        return sb.toString();
+    }
 
-    public String toSingleByteAlphabetNumberMark(String target);
+    public String toSingleByteAlphabetNumber(String target) {
+        if (target == null) {
+            return target;
+        }
+        final StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < target.length(); i++) {
+            final char currentChar = target.charAt(i);
+
+            if (currentChar >= 0xff10 && currentChar <= 0xff19) {
+                sb.append(toSingleByteCharacter(currentChar));
+            } else if (currentChar >= 0xff21 && currentChar <= 0xff3a) {
+                sb.append(toSingleByteCharacter(currentChar));
+            } else if (currentChar >= 0xff41 && currentChar <= 0xff5a) {
+                sb.append(toSingleByteCharacter(currentChar));
+            } else {
+                sb.append(currentChar);
+            }
+        }
+        return sb.toString();
+    }
+
+    public String toSingleByteAlphabetNumberMark(String target) {
+        if (target == null) {
+            return target;
+        }
+        final StringBuffer sb = new StringBuffer(target.length());
+        for (int i = 0; i < target.length(); i++) {
+            final char currentChar = target.charAt(i);
+            if (currentChar >= 0xff01 && currentChar <= 0xff5e) {
+                sb.append(toSingleByteCharacter(currentChar));
+
+                // It needs to  append more mark...
+            } else if (currentChar == '\u2019' || currentChar == '\u2018' || currentChar == '\u2032') {
+                sb.append('\'');
+            } else if (currentChar == '\u201d' || currentChar == '\u201c' || currentChar == '\u2033') {
+                sb.append('\"');
+            } else if (currentChar == '\uffe5') {
+                sb.append('\\');
+            } else if (currentChar == '\u2010') {
+                sb.append('-');
+            } else if (currentChar == '\uff5e') {
+                sb.append('~');
+            } else {
+                sb.append(currentChar);
+            }
+        }
+        return sb.toString();
+    }
+
+    protected char toSingleByteCharacter(final char currentChar) {
+        return (char) (currentChar - 0xfee0);
+    }
 }
