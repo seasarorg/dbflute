6a619025ff706d1837ec45d3a21f81a51aa7bd7e
jflute <dbflute@gmail.com>
2011-05-27 05:35:43 +0000

    (DBFLUTE-827){Java/C#}: DocタスクのDataXlsTemplateでFK順序の考慮をもっと精度高く

--- dbflute/src/main/java/org/seasar/dbflute/logic/doc/dataxls/DfTableOrderAnalyzer.java
+++ dbflute/src/main/java/org/seasar/dbflute/logic/doc/dataxls/DfTableOrderAnalyzer.java
@@ -1,13 +1,16 @@
 package org.seasar.dbflute.logic.doc.dataxls;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 import org.apache.torque.engine.database.model.Database;
 import org.apache.torque.engine.database.model.ForeignKey;
 import org.apache.torque.engine.database.model.Table;
+import org.seasar.dbflute.util.Srl;
 
 /**
  * @author jflute
@@ -19,20 +22,86 @@ public class DfTableOrderAnalyzer {
         final Set<String> alreadyRegisteredSet = new HashSet<String>();
         final List<List<Table>> outputOrderedList = new ArrayList<List<Table>>();
 
-        List<Table> tableList = database.getTableList();
+        List<Table> workTableList;
+        {
+            final List<Table> allTableList = database.getTableList();
+            final TreeSet<Table> allTableSet = new TreeSet<Table>(new Comparator<Table>() {
+                public int compare(Table o1, Table o2) {
+                    // e.g. order, order_detail, order_detail_more, ...
+                    return o1.getName().compareTo(o2.getName());
+                }
+            });
+            allTableSet.addAll(allTableList);
+            workTableList = new ArrayList<Table>(allTableSet);
+        }
+        int level = 1;
         while (true) {
-            final int beforeSize = tableList.size();
-            tableList = doAnalyzeOrder(tableList, alreadyRegisteredSet, outputOrderedList);
-            if (tableList.isEmpty()) {
+            final int beforeSize = workTableList.size();
+            workTableList = doAnalyzeOrder(workTableList, alreadyRegisteredSet, outputOrderedList, level);
+            if (workTableList.isEmpty()) {
                 break;
             }
-            final int afterSize = tableList.size();
+            final int afterSize = workTableList.size();
             if (beforeSize == afterSize) { // means it cannot analyze more
-                outputOrderedList.add(tableList);
-                break;
+                if (level == 1) {
+                    ++level; // next: ignores additional foreign key
+                } else {
+                    outputOrderedList.add(workTableList);
+                    break;
+                }
             }
         }
-        return outputOrderedList;
+        return groupingLargeSection(outputOrderedList);
+    }
+
+    protected List<List<Table>> groupingLargeSection(List<List<Table>> outputOrderedList) {
+        final List<List<Table>> groupedList = new ArrayList<List<Table>>();
+        for (List<Table> tableList : outputOrderedList) {
+            if (tableList.size() < 10) {
+                groupedList.add(tableList);
+                continue;
+            }
+            List<Table> workTableList = new ArrayList<Table>(); // as initial instance
+            String currentPrefix = null;
+            for (Table table : tableList) {
+                final String name = table.getName();
+                if (currentPrefix != null) {
+                    if (name.startsWith(currentPrefix)) { // grouped
+                        workTableList.add(table);
+                    } else {
+                        if (workTableList.size() > 1) { // switched
+                            groupedList.add(workTableList);
+                            workTableList = new ArrayList<Table>();
+                        }
+                        currentPrefix = null;
+                    }
+                }
+                if (currentPrefix == null) {
+                    currentPrefix = Srl.substringFirstFront(name, "_");
+                    workTableList.add(table);
+                }
+            }
+            if (!workTableList.isEmpty()) {
+                groupedList.add(workTableList);
+            }
+        }
+
+        // assert here
+        int resourceCount = 0;
+        for (List<Table> tableList : outputOrderedList) {
+            resourceCount = resourceCount + tableList.size();
+        }
+        int groupedCount = 0;
+        for (List<Table> tableList : groupedList) {
+            groupedCount = groupedCount + tableList.size();
+        }
+        if (resourceCount != groupedCount) {
+            String msg = "The grouping process had the loss:";
+            msg = msg + " resourceCount=" + resourceCount + " groupedCount=" + groupedCount;
+            throw new IllegalStateException(msg);
+        }
+
+        return groupedList;
     }
 
     /**
@@ -42,7 +111,7 @@ public class DfTableOrderAnalyzer {
      * @return The list of unregistered table. (NotNull)
      */
     protected List<Table> doAnalyzeOrder(List<Table> tableList, Set<String> alreadyRegisteredSet,
-            List<List<Table>> outputOrderedList) {
+            List<List<Table>> outputOrderedList, final int level) {
         final List<Table> unregisteredTableList = new ArrayList<Table>();
         final List<Table> elementList = new ArrayList<Table>();
         for (Table table : tableList) {
@@ -55,6 +124,12 @@ public class DfTableOrderAnalyzer {
             boolean independent = true;
             for (ForeignKey fk : foreignKeyList) {
                 final String foreignTableName = fk.getForeignTableName();
+                if (level == 1 && fk.hasFixedCondition()) {
+                    continue;
+                }
+                if (level == 2 && fk.isAdditionalForeignKey()) {
+                    continue;
+                }
                 if (!fk.isSelfReference() && !alreadyRegisteredSet.contains(foreignTableName)) {
                     // found parent non-registered
                     independent = false;
