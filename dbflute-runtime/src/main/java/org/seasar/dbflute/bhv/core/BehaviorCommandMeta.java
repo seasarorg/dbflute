11c6bc6251609b702dc7a1a2fb7d391c85fbb035
jflute <dbflute@gmail.com>
2012-05-10 08:35:35 +0000

    Refactor for analyzing

--- dbflute-runtime/src/main/java/org/seasar/dbflute/bhv/core/BehaviorCommandMeta.java
+++ dbflute-runtime/src/main/java/org/seasar/dbflute/bhv/core/BehaviorCommandMeta.java
@@ -15,6 +15,9 @@
  */
 package org.seasar.dbflute.bhv.core;
 
+import org.seasar.dbflute.cbean.ConditionBean;
+import org.seasar.dbflute.outsidesql.OutsideSqlOption;
+
 /**
  * The meta information interface of behavior commands. <br />
  * You can get what the behavior command is.
@@ -122,4 +125,31 @@ public interface BehaviorCommandMeta {
      * @return The determination, true or false.
      */
     boolean isDelete();
+
+    // ===================================================================================
+    //                                                                Argument Information
+    //                                                                ====================
+    /**
+     * Get the instance of condition-bean specified as argument if it exists.
+     * @return The instance of condition-bean. (NullAllowed)
+     */
+    ConditionBean getConditionBean();
+
+    /**
+     * Get the path of outside-SQL if it's outside-SQL.
+     * @return The path of outside-SQL. (NullAllowed)
+     */
+    String getOutsideSqlPath();
+
+    /**
+     * Get the parameter-bean for outside-SQL if it's outside-SQL.
+     * @return The parameter-bean for outside-SQL. (NullAllowed)
+     */
+    Object getParameterBean();
+
+    /**
+     * Get the option of outside-SQL if it's outside-SQL.
+     * @return The option of outside-SQL. (NullAllowed)
+     */
+    OutsideSqlOption getOutsideSqlOption();
 }
