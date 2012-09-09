bdb1ac0cd060681004604cf2775daa45b7f65f44
jflute <dbflute@gmail.com>
2012-09-09 05:51:23 +0000

    Release DBFlute-0.9.9.8

--- dbflute/deploy-script/all-deploy.bat
+++
@@ -1,9 +0,0 @@
-cd %~p0
-cd ..
-
-call .\embedded\ant\bin\ant -f build.xml reflect-to-all-for-win
-call .\embedded\ant\bin\ant -f buildnet.xml reflect-to-basic
-call .\embedded\ant\bin\ant -f buildnet.xml reflect-to-multipledb-quill
-call .\embedded\ant\bin\ant -f buildnet.xml reflect-to-asp.net
-
-pause
