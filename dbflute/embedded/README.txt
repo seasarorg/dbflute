62cf38b603d2685a63010ab4a95117a08bb88cb4
jflute <dbflute@gmail.com>
2012-09-11 11:34:38 +0000

    Adjust License info

---
+++ dbflute/embedded/README.txt
@@ -0,0 +1,19 @@
+
+# ========================================================================================
+#                                                                                 Overview
+#                                                                                 ========
+This is the DBFlute Module to generate classes for your DBFlute programming.
+
+
+# ========================================================================================
+#                                                                              Environment
+#                                                                              ===========
+[DBFlute Module]
+ |-ant  // Apache Ant 1.7.0
+ |-etc
+ |-lib
+ |-mark
+ |-templates
+ |-build-torque.xml
+ 
+TODO jflute now writing
--- dbflute/src/main/java/org/apache/torque/engine/EngineException.java
+++ dbflute/src/main/java/org/apache/torque/engine/EngineException.java
@@ -1,6 +1,23 @@
-package org.apache.torque.engine;
-
-/* ====================================================================
+/*
+ * Copyright 2004-2012 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ *
+ * And the following license definition is for Apache Torque.
+ * DBFlute modified this source code and redistribute as same license 'Apache'.
+ * /- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ *
+ * ====================================================================
  * The Apache Software License, Version 1.1
  *
  * Copyright (c) 2002 The Apache Software Foundation.  All rights
@@ -52,17 +69,26 @@ package org.apache.torque.engine;
  * individuals on behalf of the Apache Software Foundation.  For more
  * information on the Apache Software Foundation, please see
  * <http://www.apache.org/>.
+ * 
+ * - - - - - - - - - -/
  */
+package org.apache.torque.engine;
 
 import org.apache.commons.lang.exception.NestableException;
 
 /**
- * @author Modified by jflute
+ * @author modified by jflute (originated in Apache Torque)
  */
 public class EngineException extends NestableException {
 
+    // ===================================================================================
+    //                                                                          Definition
+    //                                                                          ==========
     private static final long serialVersionUID = 1L;
 
+    // ===================================================================================
+    //                                                                         Constructor
+    //                                                                         ===========
     /**
      * Constructs a new <code>EngineException</code> without specified detail message.
      */
