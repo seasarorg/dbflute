41376bb94d6d03e6df3ac852285df6995e2fe4e1
jflute <dbflute@gmail.com>
2012-12-25 15:56:58 +0000

    Release DBFlute-1.0.1-RC2

--- dbflute/deploy-script/all-deploy.sh
+++ dbflute/deploy-script/all-deploy.sh
@@ -20,13 +20,6 @@ rm ./log/*.log
 . generate.sh
 . sql2entity.sh
 
-cd ../../dbflute-lucy-example/dbflute_exampledb
-rm ./log/*.log
-. jdbc.sh
-. doc.sh
-. generate.sh
-. sql2entity.sh
-
 cd ../../dbflute-guice-example/dbflute_exampledb
 rm ./log/*.log
 . jdbc.sh
@@ -35,6 +28,13 @@ rm ./log/*.log
 . sql2entity.sh
 
 cd ../../dbflute-cdi-example/dbflute_exampledb
+rm ./log/*.log
+. jdbc.sh
+. doc.sh
+. generate.sh
+. sql2entity.sh
+
+cd ../../dbflute-lucy-example/dbflute_exampledb
 rm ./log/*.log
 . jdbc.sh
 . doc.sh
@@ -59,6 +59,12 @@ rm ./log/*.log
 
 cd ../../dbflute-oracle-example/dbflute_exampledb
 rm ./log/*.log
+# needs environment set up so kick by yourself (refresh only here)
+#. jdbc.sh
+#. doc.sh
+#. generate.sh
+#. sql2entity.sh
+#. outside-sql-test.sh
 . manage.sh refresh
 
 cd ../../dbflute-db2-example/dbflute_exampledb
@@ -69,13 +75,20 @@ rm ./log/*.log
 . sql2entity.sh
 . outside-sql-test.sh
 
-cd ../../dbflute-sqlserver-example/dbflute_exampledb
-rm ./log/*.log
+# needs environment set up so kick by yourself (refresh only here)
+#cd ../../dbflute-sqlserver-example/dbflute_exampledb
+#rm ./log/*.log
+#. jdbc.sh
+#. doc.sh
+#. generate.sh
+#. sql2entity.sh
+#. outside-sql-test.sh
 . manage.sh refresh
 
-cd ../../dbflute-msaccess-example/dbflute_exampledb
-rm ./log/*.log
-. manage.sh refresh
+# cannot do on Mac
+#cd ../../dbflute-msaccess-example/dbflute_exampledb
+#rm ./log/*.log
+#. manage.sh refresh
 
 cd ../../dbflute-multipledb-seasar-example/dbflute_librarydb
 rm ./log/*.log
@@ -148,7 +161,7 @@ cd ../../dbflute-postgresql-example/dbflute_exampledb
 . bhvap-sql2entity.sh
 . bhvap-outside-sql-test.sh
 
-cd ../../dbflute-tricky-example/dbflute_exampledb
+cd ../../dbflute-sqlite-example/dbflute_exampledb
 . bhvap-doc.sh
 . bhvap-generate.sh
 . bhvap-sql2entity.sh
--- dbflute/deploy-script/vm-Lidoisle-deploy.sh
+++ dbflute/deploy-script/vm-Lidoisle-deploy.sh
@@ -3,8 +3,4 @@ ant -f build.xml reflect-to-oracle
 
 cd ../dbflute-oracle-example/dbflute_exampledb
 rm ./log/*.log
-. jdbc.sh
-. doc.sh
-. generate.sh
-. sql2entity.sh
-. outside-sql-test.sh
+. manage.sh renewal
