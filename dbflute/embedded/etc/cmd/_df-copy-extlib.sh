eb10ef3129eb696ab9d7c3f33f8e819916e28a04
jflute <dbflute@gmail.com>
2014-08-30 22:14:40 +0900

    (DBFLUTE-1147){Java/C#}: extlibで二つ以上のjarファイルだとコピーされない

--- dbflute/embedded/etc/cmd/_df-copy-extlib.sh
+++ dbflute/embedded/etc/cmd/_df-copy-extlib.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-if [ -e ./extlib/*.jar ]; then
-  cp -Rf ./extlib $DBFLUTE_HOME/lib/extlib
+if [ -e ./extlib ]; then
+  if [ "$(ls ./extlib | grep '.jar$')" != '' ]; then
+    cp -Rf ./extlib $DBFLUTE_HOME/lib/extlib
+  fi
 fi
