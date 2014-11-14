25a23f5b9eca0d85aebea7ca1b8d93da4f8cd29e
jflute <dbflute@gmail.com>
2014-11-15 00:03:23 +0900

    dbflute

--- dbflute/deploy-script/vm-Lidoisle-deploy.sh
+++ dbflute/deploy-script/vm-Lidoisle-deploy.sh
@@ -3,5 +3,6 @@ ant -f build.xml reflect-to-oracle
 
 cd ../../dbflute-example-database/dbflute-oracle-example/dbflute_exampledb
 rm ./log/*.log
+. nextschema-renewal.sh
 . manage.sh renewal
 . diffworld-test.sh
